dc0a8b019330e64a91ee7365ca0cf88a3e1ea98a
swiftcode <mpc@junct.com>
2021-10-27 11:22:11 -0500

    Initial Commit

---
+++ BetaUITests/BetaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BetaUITestsLaunchTests.swift
+//  BetaUITests
+//
+//  Created by mpc on 10/27/21.
+//
+
+import XCTest
+
+class BetaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
